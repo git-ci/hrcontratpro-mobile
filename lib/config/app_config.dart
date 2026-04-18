c87825e64ac06c4b5e25e54c4a6c2f472d503e8d
Ange BINDIE <angelissdev@gmail.com>
2026-04-18 19:07:03 +0000

    Config API

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -1,5 +1,4 @@
 class AppConfig {
   // URL de base du serveur Laravel (sans slash final)
-  // static const String apiBaseUrl = 'https://hrcontratpro.git-ci.com';
-  static const String apiBaseUrl = 'http://192.168.100.3:8000';
+  static const String apiBaseUrl = 'https://hrcontratpro.git-ci.com';
 }
